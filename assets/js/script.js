155c353c1f74e61d6ea3908fb57481ffb2598bd7
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-09-09 12:06:18 -0400

    pushing updates to the edits branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 fetch(https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all)
+
+
+fetch(https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all)
+    .then(response => {
+    console.log(response);
+    return response.blob();
+    })
+    .then(response => {
+    Console.log(response);
+    });
+            
+              
