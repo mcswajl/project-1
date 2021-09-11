bd2d50225b24d84b17efde2852c094b7f700a6d7
unknown <papolindo93@gmail.com>
2021-09-11 16:28:07 -0400

    new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,52 +20,47 @@ localStorage.setItem("email", email);
 
 
 
-
+const dogButton = document.getElementById("dogButton")
 
 
 // Dogs API call
 var getDogFacts = function (user) {
-var apiUrl = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all' + user + '/repos';
+var apiUrl = 'https://catfact.ninja/breeds'
+
 
 // make a get request to url
 fetch(apiUrl)
 .then(function(response) {
   // request was successful
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function(data) {
-      //console.log(data);
-      displayRepos(data, user);
-    });
-  } else {
-    alert('Error: GitHub User Not Found');
-  }
-})
-.catch(function(error) {
-  alert('Unable to connect to GitHub');
-});
+  return response.json()
+}) .then(function(data){
+  console.log(data)
 
+  var response = document.getElementById("facts")
+  console.log(response)
+
+  response.innerHTML = ''
+
+  const newDiv = document.createElement("div")
+
+  newDiv.setAttribute('src', data[0]);
+
+  response.appendChild(newDiv)
+  
+  
+  // function addElement () {
+  //   const newDiv = document.createElement("div")
+  //   const newContent = document.createTextNode("Hello")
+  //   newDiv.appendChild(newContent);
+  //   const currentDiv = document.getElementById("facts about dogs");
+  //   document.body.insertBefore(newDiv, currentDiv);
+  // }
+}) 
 }
-// Cats API call
-var getCatFacts = function (user) {
-    var apiUrl = 'https://catfact.ninja/breeds' + user + '/repos';
+// }
+// // Cats API call
+// var getCatFacts = function (user) {
+//     var apiUrl = 'https://catfact.ninja/breeds' + user + '/repos';
+  
+dogButton.addEventListener('click', getDogFacts)
 
-// make a get request to url
-fetch(apiUrl)
-.then(function(response) {
-  // request was successful
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function(data) {
-      //console.log(data);
-      displayRepos(data, user);
-    });
-  } else {
-    alert('Error: GitHub User Not Found');
-  }
-})
-.catch(function(error) {
-  alert('Unable to connect to GitHub');
-});
-
-}
