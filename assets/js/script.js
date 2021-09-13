7f847ad67dd2ba6681c6d4f1b893bced4eedfa93
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-09-13 14:35:21 -0400

    pushing my updates so the other team members can do a git pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const messageInput = document.getElementById("exampleMessage")
 const emailInput = document.getElementById("exampleEmailInput")
 const reasonInput = document.getElementById("exampleRecipientInput")
 const submit = document.getElementById("submit")
+const h3 = document.getElementById("display API response")
 
 submit.addEventListener('click', function(event) {
   event.preventDefault();
@@ -26,21 +27,20 @@ const dogButton = document.getElementById("dogButton")
 
 // Dogs API call
 var getDogFacts = function (user) {
-var apiUrl = 'https://catfact.ninja/breeds'
-
-
+var apiURL = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all'
 // make a get request to url
-fetch(apiUrl)
-.then(function(response) {
-  // request was successful
-  return response.json()
+fetch(apiURL)
+.then(function(response){
+  response.json()
 }) .then(function(data){
   console.log(data)
+  //displayFacts(data);
 
-  var response = document.getElementById("facts")
+
+  var response = document.getElementById("facts about dogs")
   console.log(response)
 
-  response.innerHTML = ''
+  response.innerHTML = data
 
   const newDiv = document.createElement("div")
 
@@ -60,6 +60,35 @@ fetch(apiUrl)
 }
 // }
 // // Cats API call
+// Dogs API call
+var getCatFacts = function (user) {
+var apiUrl = 'https://catfact.ninja/breeds'
+  
+  // make a get request to url
+  fetch(apiURL)
+  .then(function(response){
+    response.json()
+  }) .then(function(data){
+    console.log(data)
+    //displayFacts(data);
+  
+  
+    var response = document.getElementById("facts about cats")
+    console.log(response)
+  
+    response.innerHTML = data
+  
+    const newDiv = document.createElement("div")
+  
+    newDiv.setAttribute('src', data[0]);
+  
+    response.appendChild(newDiv)
+    
+    
+  }) 
+  }
+
+
 // var getCatFacts = function (user) {
 //     var apiUrl = 'https://catfact.ninja/breeds' + user + '/repos';
   
